87d2ed6b7c54de6b399bfc1e4cf99cc03b72e061
Akumaa <valoumar0711@gmail.com>
2025-09-17 14:58:31 +0200

    [UPDATE] Creation canva et quelques element graphiques et ajout element graphiques
    Co-authored-by: Tgby270 <Tgby270@users.noreply.github.com>

---
+++ JS/script.js
@@ -0,0 +1,22 @@
+launch = document.getElementById("launch");
+closeBtn = document.getElementById("close");
+canva = document.getElementById("pong");
+score = document.getElementById("score");
+
+launch.addEventListener("click", () => {launchgame();});
+
+
+//Function to launch the game
+function launchgame(){
+    //alert("Lancement de la partie");//temp
+}
+
+//Fucntion to stop the game
+function closegame(){}
+
+//Function to move the paddle on the right
+function onRight(){}
+
+//Function to move the paddle on the left
+function onLeft(){}
+
