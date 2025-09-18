45ea63696089a34df6ef68b9749c124acf569dd4
GrossinT <etupass@etu.unicaen.fr>
2025-09-18 16:17:39 +0200

    [UPDATE] only able to move platform when game started, creation  of sleep(done), handleTimer and handleEndgame function to complete

--- JS/script.js
+++ JS/script.js
@@ -5,7 +5,6 @@ score = document.getElementById("score");
 left = document.getElementById("left");
 right = document.getElementById("right");
 
-
 gameIsOn = false;
 
 launch.addEventListener("click", () => {
@@ -13,43 +12,51 @@ launch.addEventListener("click", () => {
 });
 
 closebtn.addEventListener("click", () => {
-    closegame();
-}); 
-
+  closegame();
+});
 
 right.addEventListener("mousedown", () => {
-    rightInterval = setInterval(onRight, 50);
+  rightInterval = setInterval(onRight, 50);
 });
 
 right.addEventListener("mouseup", () => {
-   clearInterval(rightInterval);
+  clearInterval(rightInterval);
 });
 
-
-
 left.addEventListener("mousedown", () => {
-   leftInterval = setInterval(onLeft, 50);
+  leftInterval = setInterval(onLeft, 50);
 });
 
 left.addEventListener("mouseup", () => {
-   clearInterval(leftInterval);
+  clearInterval(leftInterval);
 });
 
 //Function to launch the game
 function launchgame() {
-
-    paddleOX = 300;
-    paddleOY = 380;
-    drawPaddle(paddleOX, paddleOY);
-    console.log("Game started");
-    gameIsOn = true;
- 
+  paddleOX = 300;
+  paddleOY = 380;
+  drawPaddle(paddleOX, paddleOY);
+  console.log("Game started");
+  gameIsOn = true;
+  handleTimer(true);
   //alert("Lancement de la partie");//temp
+}
+
+
+function handleTimer(val){
+    if(val == false)
+    {
+        sc = 0;
+        score.textContent = "Score: " + sc + "s";
+    }
+}
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
 }
 
 //Function to draw the paddle
 function drawPaddle(x, y) {
-
   ctx = canva.getContext("2d");
   ctx.fillStyle = "black";
   paddleSizeX = 200;
@@ -59,30 +66,41 @@ function drawPaddle(x, y) {
 
 //Fucntion to stop the game
 function closegame() {
-    ctx = canva.getContext("2d");
-    ctx.clearRect(0, 0, canva.width, canva.height);  
-    gameIsOn = false;
+  ctx = canva.getContext("2d");
+  ctx.clearRect(0, 0, canva.width, canva.height);
+  gameIsOn = false;
+  handleTimer(false);
 }
 
 //Function to move the paddle on the right
 function onRight() {
+  if (gameIsOn == true) {
     ctx = canva.getContext("2d");
     newPaddleX = paddleOX + 30;
     if (newPaddleX + paddleSizeX <= canva.width) {
-        ctx.clearRect(paddleOX, paddleOY, paddleSizeX, paddleSizeY);  
-        drawPaddle(newPaddleX, paddleOY);
-        paddleOX = newPaddleX;
+      ctx.clearRect(paddleOX, paddleOY, paddleSizeX, paddleSizeY);
+      drawPaddle(newPaddleX, paddleOY);
+      paddleOX = newPaddleX;
     }
-
+  }
 }
 
 //Function to move the paddle on the left
 function onLeft() {
+  if (gameIsOn == true) {
     ctx = canva.getContext("2d");
     newPaddleX = paddleOX - 30;
     if (newPaddleX >= 0) {
-        ctx.clearRect(paddleOX, paddleOY, paddleSizeX, paddleSizeY);  
-        drawPaddle(newPaddleX, paddleOY);
-        paddleOX = newPaddleX;
+      ctx.clearRect(paddleOX, paddleOY, paddleSizeX, paddleSizeY);
+      drawPaddle(newPaddleX, paddleOY);
+      paddleOX = newPaddleX;
     }
+  }
+}
+
+function handleEndagme()
+{
+  alert("You lost the Game ! \n You'll do better next time.");
+  
+  handleTimer(false);
 }
